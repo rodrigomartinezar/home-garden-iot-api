ef4b9c33dd8bbfae2ea936b60bd662d059ff714a
rodrigomartinezar <rodrigomartinezaraya97@gmail.com>
2020-12-13 11:15:00 -0300

    remove print of query param

--- main.js
+++ main.js
@@ -97,7 +97,6 @@ app.get("/sensors-ids", (req, res) => {
 
 app.get("/city-data", (req, res) => {
   const { sensor_id } = req.query;
-  console.log("RECEIVED: ", sensor_id);
   const sensorData = fetchSensorData(sensor_id)
     .then((fetchedDataContent) => res.send(fetchedDataContent))
     .catch((err) => res.send(err));
